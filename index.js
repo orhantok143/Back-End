652c541c6aa97aaf287b8f2eba8b1022d5928278
orhantok143 <119065177+orhantok143@users.noreply.github.com>
2024-03-12 02:33:01 +0300

    6

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser")
 const bodyParser = require("body-parser")
 const morgan = require("morgan")
 const categoryRouter = require("./routers/prodCategoryRouters")
-// const cors = require("cors")
+const cors = require("cors")
 // const { newCors } = require("./middlewares/authMiddleware")
 const app = express()
 
@@ -17,13 +17,18 @@ const PORT = process.env.PORT || 4000
 
 // DB connection
 connection()
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://flamingodb.netlify.app, https://flamingo-mn.netlify.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+// Enable CORS for specific origins (replace with your actual domain names)
+const allowedOrigins = ['https://flamingodb.netlify.app', 'https://flamingo-mn.netlify.app'];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var message = 'This request is not allowed from origin: ' + origin;
+            return callback(new Error(message), false);
+        }
+        callback(null, true);
+    }
+}));
 
 
 app.use(morgan("dev"))
