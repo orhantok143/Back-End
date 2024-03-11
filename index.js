015222cde5b2acf8964dcc4b8eeec8a200623e31
orhantok143 <119065177+orhantok143@users.noreply.github.com>
2024-03-12 02:47:15 +0300

    8

--- index.js
+++ index.js
@@ -17,26 +17,26 @@ const PORT = process.env.PORT || 4000
 // DB connection
 connection()
 // Enable CORS for specific origins(replace with your actual domain names)
-const allowedOrigins = ['https://flamingodb.netlify.app', 'https://flamingo-mn.netlify.app'];
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var message = 'This request is not allowed from origin: ' + origin;
-            return callback(new Error(message), false);
-        }
-        callback(null, true);
-    }
-}));
+// const allowedOrigins = ['https://flamingodb.netlify.app', 'https://flamingo-mn.netlify.app'];
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) {
+//             var message = 'This request is not allowed from origin: ' + origin;
+//             return callback(new Error(message), false);
+//         }
+//         callback(null, true);
+//     }
+// }));
 
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', 'https://flamingodb.netlify.app');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//     res.setHeader('Access-Control-Allow-Credentials', true); // Added line
-//     next();
-// });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://flamingodb.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true); // Added line
+    next();
+});
 
 app.use(morgan("dev"))
 app.use(bodyParser.json({ limit: '100mb' }));
