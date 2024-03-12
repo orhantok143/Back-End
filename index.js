8b170be51e78bfecbbda21501f40009799b98f1d
orhantok143 <119065177+orhantok143@users.noreply.github.com>
2024-03-12 03:02:02 +0300

    11

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ connection()
 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://flamingodb.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://flamingodb.netlify.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, withCredentials');
     res.setHeader('Access-Control-Allow-Credentials', true); // Added line
